bf410f5534fc98ddd329d3553b5bafde22225755
Lukas Lykke Jensen <llj@moebelkompagniet.dk>
2025-04-28 14:24:55 +0200

    feat: Docker compose configuartions and deploy script

---
+++ Deploy.ps1
@@ -0,0 +1,47 @@
+# Server
+$imageName = "hangfire-app"
+$imagePath = "./$imageName.tar"
+$ip = "159.65.201.136"
+
+# Docker
+$composeFile = "docker-compose.yml"
+$remote_dockerPath = "/root/Docker/hangfire"
+
+# --------------------- MAIN ---------------------
+
+Write-Host "Deploying to IP: $ip" -ForegroundColor Green
+
+# Confirm docker is running, else exit
+$dockerInfo = docker info 2>&1
+if ($dockerInfo -match "docker daemon is not running") {
+    Write-Host "Docker is not running, please start docker and try again" -ForegroundColor Red
+    exit
+}
+else {
+    Write-Host "Docker is running" -ForegroundColor Green
+}
+
+
+Write-Host "--------- (DOCKER DEPLOYMENT) ---------" -ForegroundColor Cyan
+
+
+Write-Host "Building image: $imageName" -ForegroundColor Green
+docker compose build
+
+Write-Host "Saving image: $imageName" -ForegroundColor Green
+docker save -o $imagePath $imageName
+
+Write-Host "Copying image to server" -ForegroundColor Green
+Invoke-Expression "scp $imagePath root@$ip`:$remote_dockerPath/"
+
+Write-Host "Copying $composeFile to server" -ForegroundColor Green
+Invoke-Expression "scp $composeFile root@$ip`:$remote_dockerPath/$composeFile"
+
+Write-Host "Loading image on server" -ForegroundColor Green
+Invoke-Expression "ssh root@$ip 'docker load -i $remote_dockerPath/$imageName.tar'"
+
+Write-Host "Compose down" -ForegroundColor Green
+Invoke-Expression "ssh root@$ip 'docker compose -f $remote_dockerPath/$composeFile down'"
+
+Write-Host "Compose up" -ForegroundColor Green
+Invoke-Expression "ssh root@$ip 'docker compose -f $remote_dockerPath/$composeFile up -d'"
--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,30 @@
+# See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+# This stage is used when running from VS in fast mode (Default for Debug configuration)
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+EXPOSE 8081
+
+
+# This stage is used to build the service project
 FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
-WORKDIR /App
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["hangfire.csproj", "."]
+RUN dotnet restore "./hangfire.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "./hangfire.csproj" -c $BUILD_CONFIGURATION -o /app/build
 
-# Copy everything
-COPY . ./
-# Restore as distinct layers
-RUN dotnet restore
-# Build and publish a release
-RUN dotnet publish -o out
+# This stage is used to publish the service project to be copied to the final stage
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "./hangfire.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
 
-# Build runtime image
-FROM mcr.microsoft.com/dotnet/aspnet:9.0
-WORKDIR /App
-COPY --from=build /App/out .
-ENTRYPOINT ["dotnet", "DotNet.Docker.dll"]
+# This stage is used in production or when running from VS in regular mode (Default when not using the Debug configuration)
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "hangfire.dll"]
