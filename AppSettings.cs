d4f23465bc25c9e82fc8f8489528bfee54391032
Lukas Lykke Jensen <llj@moebelkompagniet.dk>
2025-05-05 13:43:41 +0200

    fix: Add appsettings to appsettings class

--- AppSettings.cs
+++ AppSettings.cs
@@ -5,6 +5,7 @@ public class AppSettings
 {
     public ConnectionStrings ConnectionStrings { get; set; } = null!;
     public ShopifyAccessInformation ShopifyAccessInformation { get; set; } = null!;
+    public SmartPackAccessInformation SmartPack { get; set; } = null;
 
 }
 
@@ -17,4 +18,11 @@ public class ShopifyAccessInformation
 {
     public required string Domain { get; set; }
     public required string AppPassKey { get; set; }
+}
+
+public class SmartPackAccessInformation
+{
+    public required string APIUrl { get; set; }
+    public required string AppId { get; set; }
+    public required string AppToken { get; set; }
 }
